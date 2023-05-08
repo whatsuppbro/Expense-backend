c3dda2dab20710b1debc6a4d27a8c31f012c8d73
Natee Muenchananan <natee.tee159@gmail.com>
2023-05-08 19:51:17 +0700

    update

--- models/userModel.js
+++ models/userModel.js
@@ -1,5 +1,4 @@
 const mongoose = require('mongoose');
-const bcrypt = require('bcrypt');
 
 const userSchema = new mongoose.Schema({
     name: {
@@ -21,20 +20,5 @@ const userSchema = new mongoose.Schema({
         required: true,
     }
 }, {timestamps: true})
-
-userSchema.pre('save', async function (next) {
-  // Update the updatedAt timestamp
-  this.updatedAt = new Date();
-
-  if (this.isModified('password')) {
-    try {
-      const salt = await bcrypt.genSalt(10);
-      this.password = await bcrypt.hash(this.password, salt);
-    } catch (err) {
-      return next(err);
-    }
-  }
-  next();
-});
 
 module.exports = mongoose.model('users', userSchema);
